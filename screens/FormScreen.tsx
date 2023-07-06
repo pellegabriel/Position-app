60b4071f697d627420829b3ce27f2b998bea5f5d
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2023-07-06 11:08:11 -0300

    Mapa y creacion del formulario
    Estaba probando el formulario, pero no me anda bien react-native-screens, asi que voy a crear un branch nuevo y a intentar ahi con otras alternativas

--- screens/FormScreen.tsx
+++ screens/FormScreen.tsx
@@ -1,26 +1,16 @@
 // import React, { useState } from 'react';
 // import { View, Button, TextInput } from 'react-native';
-// import { StackNavigationProp } from '@react-navigation/stack';
-// import { RouteProp } from '@react-navigation/native';
-// import { RootStackParamList, VehicleData } from '../App'; // Asegúrate de actualizar esta ruta de importación según corresponda
+// import { StackScreenProps } from '@react-navigation/stack';
 
-// type FormScreenNavigationProp = StackNavigationProp<
-//   RootStackParamList,
-//   'Form'
-// >;
-
-// type FormScreenRouteProp = RouteProp<RootStackParamList, 'Form'>;
-
-// type Props = {
-//   navigation: FormScreenNavigationProp;
-//   route: FormScreenRouteProp;
+// type RootStackParamList = {
+//   Form: undefined;
+//   Map: { vehicleData: { model: string; color: string } };
 // };
 
-// const FormScreen: React.FC<Props> = ({ navigation }) => {
-//   const [vehicleData, setVehicleData] = useState<VehicleData>({ model: '', color: '' });
+// const FormScreen = ({ navigation }: StackScreenProps<RootStackParamList, 'Form'>) => {
+//   const [vehicleData, setVehicleData] = useState({ model: '', color: '' });
 
 //   const handleSubmit = () => {
-//     // Aquí, asumamos que has recogido los datos del vehículo en vehicleData
 //     navigation.navigate('Map', { vehicleData });
 //   }
 
@@ -29,12 +19,12 @@
 //       <TextInput
 //         placeholder="Model"
 //         value={vehicleData.model}
-//         onChangeText={text => setVehicleData({...vehicleData, model: text})}
+//         onChangeText={text => setVehicleData(prevState => ({...prevState, model: text}))}
 //       />
 //       <TextInput
 //         placeholder="Color"
 //         value={vehicleData.color}
-//         onChangeText={text => setVehicleData({...vehicleData, color: text})}
+//         onChangeText={text => setVehicleData(prevState => ({...prevState, color: text}))}
 //       />
 //       <Button title="Submit" onPress={handleSubmit} />
 //     </View>
