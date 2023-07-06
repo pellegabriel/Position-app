a19c55328d2d9b9b048e7ee6ff435f0d50ae9414
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2023-07-06 00:40:43 -0300

    Arreglando problemas de build

--- App.tsx
+++ App.tsx
@@ -1,18 +1,83 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import MapScreen from './screens/MapScreen';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import MapViewDirections from 'react-native-maps-directions';
 
 const App: React.FC = () => {
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+  const destination = { latitude: -34.92158951393353, longitude: -57.952644470960294 }; // This is a fixed destination
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyBBVk4iIjHSYXgGjZA08-VKCCCbm03Z2is'; // replace with your Google Maps Directions API Key
+  const handleGeolocation = () => {
+    // if (hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+          (position) => {
+            setLongitude(position.coords.longitude)
+          },
+          (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message, 'error');
+          },
+          {
+            enableHighAccuracy: false,
+            timeout: 5000,
+            maximumAge: 10000
+        }      );
+    // }
+  }
+  useEffect(() => {
+    handleGeolocation()
+  }, []);
+
+  // if (latitude === null || longitude === null) {
+  //   return null;
+  // }
   return (
-    <SafeAreaView style={styles.container}>
-      <MapScreen />
-    </SafeAreaView>
+    <View style={styles.container}>
+      {/* <MapScreen /> */}
+        <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -34.92158951393353,
+          longitude: -57.952644470960294,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+            <Marker
+          coordinate={{
+            latitude: latitude,
+            longitude: longitude,
+          }}
+        />
+        {/* <Marker
+          coordinate={destination}
+        />  */}
+        <MapViewDirections
+          // origin={{ latitude: latitude, longitude: longitude }}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="hotpink"
+          onReady={result => {
+            console.log(`Distance: ${result.distance} km`);
+            console.log(`Duration: ${result.duration} min.`);
+          }}
+        />
+      </MapView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fafafa'
+  },
+  map: {
+    flex: 1,
+    backgroundColor: 'blue',
   },
 });
 
--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,18 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+//otra libreria
+// import MapViewDirections from 'react-native-maps-directions';
+// import { PERMISSIONS, request } from 'react-native-permissions';
 
 const MapScreen: React.FC = () => {
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
+  const destination = { latitude: -34.92158951393353, longitude: -57.952644470960294 }; // This is a fixed destination
+  // const GOOGLE_MAPS_APIKEY = 'AIzaSyBBVk4iIjHSYXgGjZA08-VKCCCbm03Z2is'; // replace with your Google Maps Directions API Key
 
-  const latitude = 37.78825; // Latitud de ejemplo
-  const longitude = -122.4324; // Longitud de ejemplo
+  const handleGeolocation = () => {
+    // if (hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position, 'funciono');
+          },
+          (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message, 'error');
+          },
+          {
+            enableHighAccuracy: false,
+            timeout: 5000,
+            maximumAge: 10000
+        }      );
+    // }
+  }
+  useEffect(() => {
+    handleGeolocation()
+  }, []);
+
+  if (latitude === null || longitude === null) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
-      <MapView
+      {/* <MapView
         style={styles.map}
-        region={{
+        initialRegion={{
           latitude: latitude,
           longitude: longitude,
           latitudeDelta: 0.0922,
@@ -25,20 +54,35 @@ const MapScreen: React.FC = () => {
             longitude: longitude,
           }}
         />
-      </MapView>
+        <Marker
+          coordinate={destination}
+        /> */}
+        {/* <MapViewDirections
+          origin={{ latitude: latitude, longitude: longitude }}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="hotpink"
+          onReady={result => {
+            console.log(`Distance: ${result.distance} km`);
+            console.log(`Duration: ${result.duration} min.`);
+          }}
+        /> */}
+      {/* </MapView> */}
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'red',
+    height: 100,
+    width: 100
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    backgroundColor: 'blue',
   },
 });
 
